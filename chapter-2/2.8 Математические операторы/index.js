a8755c506eb1cde5a75dc64a8f79d2aea5056842
kamalov-eldar <kamalov-eldar@mail.ru>
2024-11-27 10:36:27 +0300

    %

---
+++ chapter-2/2.8 Математические операторы/index.js
@@ -0,0 +1,11 @@
+// ## Взятие остатка %
+
+console.log("#### Взятие остатка %  ####");
+console.log(8.5 % 4);
+console.log(8 % 4.1);
+console.log(8.2 % 4.1);
+
+console.log(-13 % 5);
+console.log(4 % -2);
+console.log(-3n % 2n);
+console.log(3n % -2n); 
