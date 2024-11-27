7846b44efc279ef322c4327d38acd821784ca4ad
kamalov-eldar <kamalov-eldar@mail.ru>
2024-11-27 11:00:38 +0300

    **

--- chapter-2/2.8 Математические операторы/index.js
+++ chapter-2/2.8 Математические операторы/index.js
@@ -8,4 +8,7 @@ console.log(8.2 % 4.1);
 console.log(-13 % 5);
 console.log(4 % -2);
 console.log(-3n % 2n);
-console.log(3n % -2n); 
+console.log(3n % -2n);
+console.log("6n % 2", 6n % 2);
+console.log("6 % 2n", 6 % 2n);
+console.log("2 % NaN:", 2 % NaN);
