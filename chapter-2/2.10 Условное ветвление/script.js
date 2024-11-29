9ef3c183c304c861ab3e3b9fbee7c85f769fc130
kamalov-eldar <kamalov-eldar@mail.ru>
2024-11-29 16:28:55 +0300

    finish 2.11

--- chapter-2/2.10 Условное ветвление/script.js
+++ chapter-2/2.10 Условное ветвление/script.js
@@ -1,6 +1,17 @@
 console.log("------------  Условное ветвление: if, '?'  ------------");
-'Какое "официальное" название JavaScript?'
+('Какое "официальное" название JavaScript?');
 
-if(){
-    
-}
+let numeric = prompt("Введите число");
+/* if (numeric % 2 === 0) {
+    console.log("Четное");
+} else {
+    console.log("Не четное");
+} */
+
+numeric % 2 === 0 ? console.log("Четное") : console.log("Не четное");
+
+// переписать на '?'
+let message;
+
+message = login == "Сотрудник" ? "Привет" : "Здравствуйте";
+message = login == "" ? "Нет логина" : "";
