7bcd22dbadc6a2f3ab37571a156978501a9dcc3c
kamalov-eldar <kamalov-eldar@mail.ru>
2024-11-30 09:50:07 +0300

    2.11

--- chapter-2/2.11 Логические операторы/script.js
+++ chapter-2/2.11 Логические операторы/script.js
@@ -1,2 +1,6 @@
 console.log("------------  Логические операторы  ------------");
-
+console.log("«||»");
+console.log('"" || "" || "" || "last": ', "" || "" || "" || "last"); // last
+console.log("«&&»");
+console.log('1 && 1 && undefined: ', 1 && 1 && undefined); // undefined
+console.log("1 && 2 && null && 3: ", 1 && 2 && null && 3); // null
