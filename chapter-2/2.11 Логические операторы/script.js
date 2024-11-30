e69551b05fff9ca49cf4f38626bd5ad4477298a7
kamalov-eldar <kamalov-eldar@mail.ru>
2024-11-30 15:10:00 +0300

    start 2.12

--- chapter-2/2.11 Логические операторы/script.js
+++ chapter-2/2.11 Логические операторы/script.js
@@ -5,6 +5,10 @@ console.log("«||»");
 console.log("true || true: ", true || true);
 console.log("false || true: ", false || true);
 console.log("true || false: ", true || false);
+console.log("null || true: ", null || true);
+console.log("null || false: ", null || false);
+console.log("false || null:", false || null);
+console.log("true || null:", true || null);
 console.log("undefined || null || 0: ", undefined || null || 0);
 console.log('"" || "" || "" || "last": ', "" || "" || "" || "last");
 console.log("null || 2 || undefined: ", null || 2 || undefined);
@@ -33,3 +37,21 @@ console.log("«!»");
 
 console.log("!true: ", !true);
 console.log("!0: ", !0);
+
+/* let age = prompt("Введите число в диапазоне от 14 до 90 включительно");
+
+if (Number(age) >= 14 && 90 >= Number(age)) {
+    console.log("число в диапазоне от 14 до 90");
+} else console.log("число вне диапазона"); */
+
+/* let age = prompt("Введите число вне диапазона от 14 до 90 включительно");
+
+if (!(Number(age) >= 14 && 90 >= Number(age))) {
+    console.log("число вне диапазона");
+} else console.log("число в диапазоне от 14 до 90 включительно"); */
+
+/* let age = Number(prompt("Введите число вне диапазона от 14 до 90 включительно"));
+
+if (age < 14 || age > 90) {
+    console.log("число вне диапазона");
+} else console.log(" число в диапазоне от 14 до 90 включительно"); */
