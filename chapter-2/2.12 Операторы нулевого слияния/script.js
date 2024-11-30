eedd47719164b145b750510154027d8ee4440072
kamalov-eldar <kamalov-eldar@mail.ru>
2024-11-30 16:06:48 +0300

    finish 2.12

--- chapter-2/2.12 Операторы нулевого слияния/script.js
+++ chapter-2/2.12 Операторы нулевого слияния/script.js
@@ -1,2 +1,16 @@
 console.log("------------  ??  ------------");
 
+console.log("1 ?? undefined:", 1 ?? undefined);
+console.log("undefined ?? 1:", undefined ?? 1);
+console.log("1 ?? null:", 1 ?? null);
+console.log("null ?? 1:", null ?? 1);
+console.log("NaN ?? 1:", NaN ?? 1);
+console.log("false ?? 1:", false ?? 1);
+console.log("undefined ?? undefined:", undefined ?? undefined);
+console.log("undefined ?? null:", undefined ?? null);
+
+console.log("------------  ??=  ------------");
+let x;
+let y = 1;
+console.log("x ??= y:", (x ??= y));
+console.log('x: ', x);
