fd80c700dd9472102c5bd898e277917d7a9e978e
kamalov-eldar <kamalov-eldar@mail.ru>
2024-12-13 12:33:06 +0300

    fix

--- chapter-2/2.15 Функции/script.js
+++
@@ -1,9 +0,0 @@
-function func(num) {
-    console.log("func", num);
-    num = 2; // меняем локальную переменную
-    console.log("func", num);
-}
-
-let num = 1;
-func(num);
-console.log(num); // выведет 1 - ничего не поменялось
