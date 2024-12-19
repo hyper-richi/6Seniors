92477dd2f56ac77833b7add16f83a0acc593fa74
kamalov-eldar <kamalov-eldar@mail.ru>
2024-12-19 18:31:42 +0300

    fix

--- chapter-4/script.js
+++ chapter-4/script.js
@@ -1,7 +1,11 @@
 let user = {
     name: "John",
     age: 30,
+    test: undefined,
 };
+
+console.log("name" in user); // true
+console.log("test" in user); // true
 
 let clone = {}; // новый пустой объект
 
@@ -21,4 +25,26 @@ let fullName = {
 
 let admin = fullName;
 fullName = null;
-console.log("admin: ", admin);
+// console.log("admin: ", admin);
+
+let arr = [1, 2, 3, 4];
+for (key in arr) {
+    // console.log("key: ", key); // 0,1,2,3
+}
+for (prop of arr) {
+    // console.log("prop: ", prop);
+}
+
+function isEmpty(obj) {
+    for (let key in obj) {
+        console.log("key: ", key);
+
+        return false;
+    }
+    return true;
+}
+
+let obj = { value: 1 };
+
+console.log("isEmpty(obj):", isEmpty({})); // true
+console.log("isEmpty(obj):", isEmpty({ value: 1 })); // false
