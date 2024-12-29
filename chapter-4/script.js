3c9bad1018e25b486c72ff26597b246a81358f58
kamalov-eldar <kamalov-eldar@mail.ru>
2024-12-29 19:16:15 +0300

    fix

--- chapter-4/script.js
+++ chapter-4/script.js
@@ -1,50 +1,25 @@
 // user.sayHi();
+"use strict";
 
-/* const user = {
+const user = {
     name: "John",
     sayHi() {
-        let arrow = () => console.log("console-1", user);
+        let arrow = () => console.log("console-1", this);
         arrow();
     },
-    arrow: (() => console.log("console-2"))(),
-};
-user.sayHi(); */
-
-/* let calculator = {
-    result: 0,
-    num1: 1,
-    num2: 2,
-    read() {
-        return this.result;
-    },
-    sum() {
-        this.result = this.num1 + this.num2;
-    },
-    mul() {
-        this.result = this.num1 * this.num2;
-        return this.result;
+    arrow: () => {
+        this.name = "value";
+        console.log("console-2", this);
     },
 };
-console.log("calculator.mul:", calculator.mul());
-console.log("calculator.sum2:", calculator.sum());
-console.log("calculator.read:", calculator.read()); */
+user.arrow();
 
-// ladder.up().up().down().showStep().down().showStep(); // показывает 1 затем 0
-
-let ladder = {
-    step: 0,
-    up() {
-        this.step++;
-        return this;
+const obj = {
+    value: 42,
+    getValue: function () {
+        return this.value;
     },
-    down() {
-        this.step--;
-        return this;
-    },
-    showStep: function () {
-        // показывает текущую ступеньку
-        console.log(this.step);
-        return this;
-    },
+    this: this,
+    this1: this,
 };
-ladder.up().up().down().showStep().down().showStep();
+console.log("obj: ", obj);
