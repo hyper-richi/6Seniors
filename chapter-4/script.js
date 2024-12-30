8a94486423eadbeba3936dd1570828ee4f370fad
kamalov-eldar <kamalov-eldar@mail.ru>
2024-12-30 16:54:22 +0300

    fix

--- chapter-4/script.js
+++ chapter-4/script.js
@@ -4,7 +4,7 @@ const src2 = { b: 9 };
 
 const returnedTarget = Object.assign(target, src2, src1);
 
-console.log(target);
+// console.log(target);
 
 let user = {
     name: "John",
@@ -12,5 +12,30 @@ let user = {
 
 let admin = user;
 user = null;
-console.log("admin: ", admin);
-console.log("user: ", user); // null
+// console.log("admin: ", admin);
+// console.log("user: ", user); // null
+
+function marry(man, woman) {
+    woman.husband = man;
+    man.wife = woman;
+
+    return {
+        father: man,
+        mother: woman,
+    };
+}
+
+let family = marry(
+    {
+        name: "John",
+    },
+    {
+        name: "Ann",
+    },
+);
+console.log("family: ", family);
+
+delete family.father.wify;
+delete family.father;
+
+console.log("family-2: ", family);
