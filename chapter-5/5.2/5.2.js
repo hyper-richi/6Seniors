fc25a9fed47147c700bd2626e06e3aa2db890cfa
kamalov-eldar <kamalov-eldar@mail.ru>
2025-01-23 20:24:13 +0300

    LC

--- chapter-5/5.2/5.2.js
+++ chapter-5/5.2/5.2.js
@@ -1,17 +1,14 @@
-let widthTab = "100px";
 
 function parseIntCustom(str) {
     let resultStr = "";
 
     for (let i = 0; i < str.length; i++) {
-        if (Number(str[i])) {
-            if (String(str[i])) {
-                resultStr = str[i];
-                // console.log("Number(str[i]): ", Number(str[i]));
-            }
+        if (!Object.is(NaN, Number(str[i]))) {
+            resultStr += str[i];
         } else break;
     }
     return Number(resultStr);
 }
 
-console.log(parseIntCustom("100px"));
+console.log(parseIntCustom("100456px"));
+console.log(parseInt("100123px"));
