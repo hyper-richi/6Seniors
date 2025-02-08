0d2c4d7c8626be843c84cfb878b8a2c5575a9db6
kamalov-eldar <kamalov-eldar@mail.ru>
2025-02-08 16:53:18 +0300

    5.6

--- chapter-5/5.4 Массивы/5.4.js
+++ chapter-5/5.4 Массивы/5.4.js
@@ -1,23 +1,57 @@
 // "use strict";
+let arr = [1, 2, 3, [1, 2, 3, [1, 2, 3]]];
+
+console.log(arr + ""); // 1,2,3,1,2,3,1,2,3
 
 const numbersArray = [4, 12, 8, 15, 2, 25];
-console.log("numbersArray: ", numbersArray);
-
-numbersArray.getThis = function (cb) {
-  console.log("numbersArray.getThis", this);
-  cb.call(this);// привязал numbersArray
-  const getThisArrow = () => {
-    console.log("getThisArrow", this);
-  };
-  getThisArrow();
-};
-
-numbersArray.getThis(function (item) {
-  console.log("this-cb:", this); // window
-  return item > 10;
-});
 
 numbersArray.filter(function (item) {
-  8; // console.log("this-filter:", this); // window
   return item > 10;
 });
+
+numbersArray["02"] = "02";
+numbersArray["12"] = 12;
+numbersArray[-1] = -1;
+console.log("numbersArray: ", numbersArray);
+
+numbersArray[Symbol.iterator] = function () {
+  const properties = Object.keys(this);
+  let count = 0;
+
+  return {
+    next() {
+      const key = properties[count];
+      let result = { done: !(count < properties.length), value: numbersArray[key] };
+      count++;
+      return result;
+    },
+  };
+};
+
+for (let value of numbersArray) {
+  console.log(value);
+}
+
+// valueOf toString
+
+// numbersArray["toString"] = function (...args) {
+//   console.log("toString");
+// };
+
+// numbersArray["valueOf"] = function (...args) {
+//   console.log("valueOf");
+// };
+
+// numbersArray["default"] = function (...args) {
+//   console.log("default");
+// };
+
+// Symbol.toPrimitive
+
+let fruits = ["Apple", "Orange", "Plum"];
+
+// fruits[Symbol.toPrimitive] = function (hint) {
+//   console.log("hint: ", hint); // number
+// };
+
+//console.log(`${numbersArray}`);
