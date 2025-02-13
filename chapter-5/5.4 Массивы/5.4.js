b2a84dbb8f3027046aaa4a35ff7d203cc6e6a416
kamalov-eldar <kamalov-eldar@mail.ru>
2025-02-13 21:22:43 +0300

    5.4

--- chapter-5/5.4 Массивы/5.4.js
+++ chapter-5/5.4 Массивы/5.4.js
@@ -11,19 +11,19 @@ console.log(numbersArray.valueOf); // ƒ valueOf() { [native code] }
 console.log(numbersArray.toLowerCase); // undefined
 console.log("Symbol.toPrimitive: ", numbersArray[Symbol.toPrimitive]); // undefined
 
-// numbersArray["toString"] = function () {
-//   console.log("toString");
-// };
+numbersArray["toString"] = function () {
+  console.log("toString");
+};
 
-// numbersArray["valueOf"] = function () {
-//   console.log("valueOf");
-// };
+numbersArray["valueOf"] = function () {
+  console.log("valueOf");
+};
 
 // Symbol.toPrimitive;
 
-// numbersArray[Symbol.toPrimitive] = function (hint) {
-//   console.log("hint: ", hint);
-//   return "";
-// };
+numbersArray[Symbol.toPrimitive] = function (hint) {
+  console.log("hint: ", hint);
+  return "";
+};
 
 console.log(numbersArray * 1); // NaN // valueOf
