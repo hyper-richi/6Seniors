d07500ef5b1baa4b51c64b170c90fa5bb6a0abdd
kamalov-eldar <kamalov-eldar@mail.ru>
2025-03-05 14:43:22 +0300

    6.1

---
+++ 6.Работа с функциями/6.js
@@ -0,0 +1,42 @@
+function factorial(n) {
+  if (n === 1) {
+    return 1;
+  }
+  debugger;
+  return n * factorial(n - 1);
+}
+
+//console.log(factorial(5)); // 120
+// console.log(factorial(3)); // 6
+
+function sumArray(arr) {
+  const len = arr.length;
+  if (len === 0) {
+    return 0;
+  }
+
+  function recurs(count) {
+    if (count === 0) {
+      return arr[0];
+    }
+
+    debugger;
+
+    return arr[count] + recurs(count - 1); // 4+3+12+10
+  }
+
+  return recurs(len - 1);
+}
+
+//console.log(sumArray([10, 12, 3, 4]));
+
+function sumArray(arr) {
+
+  if (arr.length === 0) {
+    return 0;
+  }
+
+  return arr[0] + sumArray(arr.slice(1));
+}
+
+console.log(sumArray([10, 12, 3, 4]));
